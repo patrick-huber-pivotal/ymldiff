648957ff80a3960a44262d38b98c2ef6cf4eb874
patrickhuber <patrickhuber@users.noreply.github.com>
2020-01-30 07:34:02 -0500

    adds json parsing

--- formatters/json.go
+++ formatters/json.go
@@ -1,19 +1,28 @@
 package formatters
 
 import (
+	"encoding/json"
 	"io"
 
 	"github.com/patrick-huber-pivotal/ymldiff/diff"
 )
 
-type json struct {
+type jsonFormatter struct {
+	changeLog *diff.ChangeLog
 }
 
 // NewJSON returns a json change log formatter
 func NewJSON(changeLog *diff.ChangeLog) Formatter {
-	return &json{}
+	return &jsonFormatter{
+		changeLog: changeLog,
+	}
 }
 
-func (f *json) Write(out io.Writer) error {
-	return nil
+func (f *jsonFormatter) Write(out io.Writer) error {
+	data, err := json.Marshal(f.changeLog)
+	if err != nil {
+		return err
+	}
+	_, err = out.Write(data)
+	return err
 }
--- main.go
+++ main.go
@@ -2,51 +2,100 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/patrick-huber-pivotal/ymldiff/formatters"
+	"github.com/urfave/cli/v2"
 
 	"github.com/patrick-huber-pivotal/ymldiff/diff"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		printHelpAndExit()
+	app := &cli.App{
+		Name:  "yamldiff",
+		Usage: "extracts the differences between two yaml files",
+		CustomAppHelpTemplate: `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.HelpName}} [options] <from> <to>
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Usage:   "the format of the output. Default 'bosh'. (json, bosh, yaml)",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			fromFile := c.Args().Get(0)
+			toFile := c.Args().Get(1)
+
+			if strings.TrimSpace(fromFile) == "" {
+				return fmt.Errorf("missing <from> file argument")
+			}
+
+			if strings.TrimSpace(toFile) == "" {
+				return fmt.Errorf("missing <to> file argument")
+			}
+
+			_, err := os.Stat(fromFile)
+			if err != nil {
+				return err
+			}
+
+			_, err = os.Stat(toFile)
+			if err != nil {
+				return err
+			}
+
+			changeLog, err := diff.NewChangeLogFromFiles(fromFile, toFile)
+			if err != nil {
+				return err
+			}
+
+			format := "bosh"
+			if c.IsSet("format") {
+				format = c.String("format")
+			}
+			var formatter formatters.Formatter
+			switch format {
+			case "bosh":
+				formatter = formatters.NewBOSH(changeLog)
+				break
+			case "json":
+				formatter = formatters.NewJSON(changeLog)
+				break
+			case "yaml":
+				formatter = formatters.NewYAML(changeLog)
+				break
+			default:
+				return fmt.Errorf("unrecognized format %s", c.String("format"))
+			}
+
+			return formatter.Write(os.Stdout)
+		},
 	}
 
-	fromFile := os.Args[1]
-	toFile := os.Args[2]
-
-	_, err := os.Stat(fromFile)
+	err := app.Run(os.Args)
 	if err != nil {
-		printErrorAndExit(err.Error())
+		log.Fatal(err)
 	}
-
-	_, err = os.Stat(toFile)
-	if err != nil {
-		printErrorAndExit(err.Error())
-	}
-
-	changeLog, err := diff.NewChangeLogFromFiles(fromFile, toFile)
-	if err != nil {
-		printErrorAndExit(err.Error())
-	}
-
-	formatter := formatters.NewBOSH(changeLog)
-	formatter.Write(os.Stdout)
-}
-
-func printErrorAndExit(message string) {
-	os.Stderr.WriteString(message)
-	os.Exit(1)
-}
-
-func printHelpAndExit() {
-	printHelp()
-	os.Exit(1)
-}
-
-func printHelp() {
-	fmt.Printf("usage:\n%s <from> <to>", os.Args[0])
-	fmt.Println()
 }
