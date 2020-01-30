d61e43549ee417590ba28816bb932ecc87fe79fa
patrick-huber-pivotal <patrick-huber-pivotal@users.noreply.github.com>
2020-01-30 08:51:07 -0500

    adds json tests

--- formatters/json.go
+++ formatters/json.go
@@ -13,8 +13,20 @@ type jsonFormatter struct {
 
 // NewJSON returns a json change log formatter
 func NewJSON(changeLog *diff.ChangeLog) Formatter {
+	changes := []diff.Change{}
+	for _, change := range changeLog.Differences {
+		newChange := diff.Change{
+			From:      clean(change.From),
+			To:        clean(change.To),
+			Operation: change.Operation,
+			Path:      change.Path,
+		}
+		changes = append(changes, newChange)
+	}
 	return &jsonFormatter{
-		changeLog: changeLog,
+		changeLog: &diff.ChangeLog{
+			Differences: changes,
+		},
 	}
 }
 
@@ -25,4 +37,21 @@ func (f *jsonFormatter) Write(out io.Writer) error {
 	}
 	_, err = out.Write(data)
 	return err
+}
+
+func clean(data interface{}) interface{} {
+	switch t := data.(type) {
+	case (map[interface{}]interface{}):
+		return cleanInterfaceMap(t)
+	}
+	return data
+}
+
+func cleanInterfaceMap(data map[interface{}]interface{}) map[string]interface{} {
+	m := map[string]interface{}{}
+	for k, v := range data {
+		s := k.(string)
+		m[s] = clean(v)
+	}
+	return m
 }
